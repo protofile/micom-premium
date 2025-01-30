f2152e84bb980db453cf51645d47191a957877c8
yudantoardi <yudantoardi@gmail.com>
2025-01-30 14:54:26 +0700

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
     $(".main-menu").toggleClass("show");
     $($(this).children("i.fa")).toggleClass("fa-bars fa-times");
   });
+
+  var afs = 16;
+  $(".article-text").css("font-size", afs + "px");
+  $(".font-min").click(function () {
+    if (afs > 14) {
+      afs--;
+      $(".article-text").css("font-size", afs + "px");
+    } else {
+      //do nothing
+    }
+  });
+  $(".font-plus").click(function () {
+    if (afs < 20) {
+      afs++;
+      $(".article-text").css("font-size", afs + "px");
+    } else {
+      //do nothing
+    }
+  });
 });
